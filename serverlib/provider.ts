eb6bb5519afcc16eac5ce610c4b8a5327fda0afd
bearcattt <ewigglet8@gmail.com>
2025-11-04 21:06:08 -0400

    e

--- serverlib/provider.ts
+++ serverlib/provider.ts
@@ -1,5 +1,6 @@
 export function findProvider(port: number) {
-    const provider = process.env.RENDER_EXTERNAL_URL ||
+  const provider =
+    process.env.RENDER_EXTERNAL_URL ||
     (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) ||
     (process.env.RAILWAY_STATIC_URL && `https://${process.env.RAILWAY_STATIC_URL}`) ||
     (process.env.FLY_APP_NAME && `https://${process.env.FLY_APP_NAME}.fly.dev`) ||
@@ -12,5 +13,5 @@ export function findProvider(port: number) {
     (process.env.GLITCH_PROJECT_ID && `https://${process.env.PROJECT_DOMAIN}.glitch.me`) ||
     (process.env.HEROKU_APP_NAME && `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`) ||
     (process.env.NETLIFY_DEV === 'true' && process.env.SITE_URL);
-    return provider;
+  return provider;
 }
