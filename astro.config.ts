47ee8937533359bcf242bd35561f0cec01b9efc7
bearcattt <ewigglet8@gmail.com>
2025-11-05 09:43:12 -0400

    fix

--- astro.config.ts
+++ astro.config.ts
@@ -12,7 +12,6 @@ import { normalizePath } from 'vite';
 import type { Plugin } from 'vite';
 import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-
 import { version } from './package.json';
 
 wisp.options.wisp_version = 2;
